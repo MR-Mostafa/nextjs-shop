41231c7330ecca5489f043002f9d4972d0a72c71
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-11 04:45:35 +0330

    add roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import MainLayout from '@/layouts/MainLayout'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import MainLayout from '@/layouts/MainLayout';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+	weight: ['400', '500', '700'],
+	subsets: ['latin'],
+	style: ['normal'],
+	display: 'swap',
+	variable: '--font-roboto',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
-  )
+	return (
+		<>
+			<style jsx global>{`
+				:root {
+					--font-roboto: ${roboto.variable};
+				}
+			`}</style>
+
+			<MainLayout>
+				<Component {...pageProps} />
+			</MainLayout>
+		</>
+	);
 }
