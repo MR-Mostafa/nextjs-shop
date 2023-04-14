1bf64b5bc8084d9b775116d7d1b960ad5caca71f
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-14 13:04:11 +0330

    create star component

---
+++ src/components/Star/Star.tsx
@@ -0,0 +1,32 @@
+import cx from 'classnames';
+import { BaseTwProps } from '@/types';
+import { ReactNode, forwardRef, ElementType } from 'react';
+import { AiFillStar } from 'react-icons/ai';
+
+interface IStar {
+	rate: number | string;
+	rootClassName?: string;
+	rateClassName?: string;
+	iconClassName?: string;
+}
+
+export const Star = forwardRef<HTMLSpanElement, IStar>((props, ref) => {
+	const { rate, rootClassName, rateClassName, iconClassName, ...rest } = props;
+
+	const rootClass = cx('Star-wrapper', rootClassName);
+	const rateClass = cx('Star-rate', rateClassName);
+	const iconClass = cx('Star-icon', iconClassName);
+
+	return (
+		<div className={rootClass} {...rest}>
+			<AiFillStar className={iconClass} />
+			<span ref={ref} className={rateClass}>
+				{rate}
+			</span>
+		</div>
+	);
+});
+
+Star.displayName = 'Star';
+
+Star.defaultProps = {};
