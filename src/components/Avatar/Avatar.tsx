5fc39977fd60e636ab004cc3dd04a687e544792b
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-14 10:50:07 +0330

    create avatar component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,5 +1,31 @@
-function Avatar() {
-	return <div>Avatar</div>;
+/* eslint-disable @next/next/no-img-element */
+import cx from 'classnames';
+import { ImgProps } from 'react-html-props';
+import { forwardRef } from 'react';
+
+interface IAvatar extends Omit<ImgProps, 'className'> {
+	rootClassName?: string;
+	imageClassName?: string;
 }
 
-export default Avatar;
+export const Avatar = forwardRef<HTMLImageElement, IAvatar>((props, ref) => {
+	const { rootClassName, imageClassName, alt = 'user avatar', ...rest } = props;
+
+	const rootClass = cx('avatar-wrapper', rootClassName);
+	const imageClass = cx('avatar-image', imageClassName);
+
+	return (
+		<span className={rootClass}>
+			<img ref={ref} className={imageClass} alt={alt} {...rest} />
+		</span>
+	);
+});
+
+Avatar.displayName = 'Avatar';
+
+Avatar.defaultProps = {
+	src: '/avatar.jpg',
+	width: 24,
+	height: 24,
+	rootClassName: 'w-[24px] h-[24px]',
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Button } from './Button/Button';
 export { TextField } from './TextField/TextField';
+export { Avatar } from './Avatar/Avatar';
