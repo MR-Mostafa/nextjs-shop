e217a3d814a8a3f41541eaa4acd88018fa542b36
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-15 20:37:25 +0330

    update MainLayout

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,17 +1,20 @@
 import { ReactNode } from 'react';
+import cx from 'classnames';
 
 import { Header } from '@/features';
 
 type TPropsMainLayout = {
 	shouldShowHeader?: boolean;
 	children: ReactNode;
+	link?: string;
+	title?: ReactNode;
 };
 
-function MainLayout({ children, shouldShowHeader = true }: TPropsMainLayout) {
+function MainLayout({ children, shouldShowHeader = true, link, title }: TPropsMainLayout) {
 	return (
 		<>
-			{shouldShowHeader && <Header />}
-			<main className="w-full p-6">{children}</main>
+			{shouldShowHeader && <Header link={link!} title={title!} />}
+			<main className={cx('w-full', { 'p-6': shouldShowHeader, 'px-6': !shouldShowHeader })}>{children}</main>
 		</>
 	);
 }
