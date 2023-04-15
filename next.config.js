17384bda8935f91df87193590f10df64d03ccac4
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-16 01:44:10 +0330

    add image remote

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
 	reactStrictMode: true,
 	generateEtags: false,
 	poweredByHeader: false,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'i.dummyjson.com',
+				port: '',
+				pathname: '/data/products/**',
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
