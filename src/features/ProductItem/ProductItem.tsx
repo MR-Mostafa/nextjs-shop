5442b221ab1a286070f375f77e5ae3a6c0bb14d1
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-04-17 06:30:12 +0330

    update ProductItem

--- src/features/ProductItem/ProductItem.tsx
+++ src/features/ProductItem/ProductItem.tsx
@@ -4,20 +4,29 @@ import { ProductItem as ProductItemType } from '@/types';
 import Image from 'next/image';
 import { shimmer, toBase64 } from '@/utils';
 import { useRouter } from 'next/router';
+import { ReactNode, memo } from 'react';
 
 interface ProductItemProps {
+	children?: ReactNode;
 	product: ProductItemType;
+	shouldPreventClick?: boolean;
 }
 
-export const ProductItem = ({ product }: ProductItemProps) => {
+export const ProductItem = memo(({ children, product, shouldPreventClick = false }: ProductItemProps) => {
 	const router = useRouter();
 
 	const link = `/products/${product.id}`;
 
 	return (
-		<Card.Wrapper onClick={() => router.push(link)}>
+		<Card.Wrapper
+			onClick={() => {
+				if (shouldPreventClick) return;
+
+				router.push(link);
+			}}
+		>
 			<Card.Image className="max-h-[335px]">
-				<Link href={link}>
+				{shouldPreventClick ? (
 					<Image
 						src={product.thumbnail}
 						alt={product.title}
@@ -26,21 +35,40 @@ export const ProductItem = ({ product }: ProductItemProps) => {
 						placeholder="blur"
 						blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(500, 335))}`}
 					/>
-				</Link>
+				) : (
+					<Link href={link}>
+						<Image
+							src={product.thumbnail}
+							alt={product.title}
+							width={500}
+							height={335}
+							placeholder="blur"
+							blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(500, 335))}`}
+						/>
+					</Link>
+				)}
 			</Card.Image>
 
 			<Card.Body className="px-1 py-2 text-left">
 				<div className="flex items-center justify-between">
-					<Link href={link} className="title">
-						{product.title}
-					</Link>
+					{shouldPreventClick ? (
+						<p className="title">{product.title}</p>
+					) : (
+						<Link href={link} className="title">
+							{product.title}
+						</Link>
+					)}
 					<p className="price">${product.price}</p>
 				</div>
 
 				<p className="description pb-3">{product.description}</p>
 
 				<Star rate={product.rating} />
+
+				{children && children}
 			</Card.Body>
 		</Card.Wrapper>
 	);
-};
+});
+
+ProductItem.displayName = 'ProductItem';
