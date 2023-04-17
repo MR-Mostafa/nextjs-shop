455bc8173a46c46d6f67b6e156f546605d4e5484
Mostafa Rahmati <M.Rahmati70@hotmail.com>
2023-04-17 12:17:25 +0330

    create cart page

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,3 +1,4 @@
 export { ProductItem } from './ProductItem/ProductItem';
 export { Header } from './Header/Header';
 export { LoginForm } from './LoginForm/LoginForm';
+export { SearchItem } from './SearchItem/SearchItem';
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,3 +1,39 @@
-export default function Cart() {
-	return <div className="w-full">Cart Page</div>;
-}
+import { Button, Counter } from '@/components';
+import MainLayout from '@/layouts/MainLayout';
+import { BiChevronLeft } from 'react-icons/bi';
+import { SearchItem } from '@/features';
+import { addItemInCart, getCartInfo } from '@/store';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+
+const Cart = () => {
+	const cartInfo = useRecoilValue(getCartInfo);
+	const updateCart = useSetRecoilState(addItemInCart);
+
+	return (
+		<MainLayout
+			shouldShowHeader
+			link="/"
+			title={
+				<Button as="a" variant="link" className="p-1 text-lg font-bold" leftIcon={<BiChevronLeft />}>
+					My Cart
+				</Button>
+			}
+		>
+			{cartInfo.items.length === 0 && <p className="pt-3 text-center font-bold">The shopping cart is empty</p>}
+
+			{cartInfo.items.length &&
+				cartInfo.items.map((item) => {
+					return (
+						<SearchItem product={item} key={item.id}>
+							<div className="flex items-center justify-between">
+								<p className="price pe-3">${item.price}</p>
+								<Counter value={item.count} setValue={(val) => {}} maxValue={item.stock} />
+							</div>
+						</SearchItem>
+					);
+				})}
+		</MainLayout>
+	);
+};
+
+export default Cart;
